e87d757432625193c5895357204735112dcba18d
Isaac Hodes <ihodes@mac.com>
2013-03-05 13:45:30 -0500

    use postgres locally, too

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
 
-SECRET_KEY=':\x90[\xf2F[X\xcbA\xcbA\xcf\xd7U3\xdb+/b\xf0\x1f\xe9\x00\xe4'
-DEBUG=True
-DATABASE_URI=os.environ.get("HEROKU_POSTGRESQL_AMBER_URL", 'sqlite:///dev.db')
-HOST=os.environ.get("HOST", "0.0.0.0")
-PORT=int(os.environ.get('PORT', 5000))
+SECRET_KEY=os.environ.get("SECRET_KEY")
+DATABASE_URI=os.environ.get("HEROKU_POSTGRESQL_AMBER_URL")
+HOST=os.environ.get("HOST")
+PORT=int(os.environ.get("PORT"))
+
+DEBUG=os.environ.get("DEBUG", False)
 
 del os
