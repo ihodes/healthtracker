475f3a6fa5c267961f2e6d9c76d6b15db5959d23
Isaac Hodes <ihodes@mac.com>
2013-03-20 22:29:04 -1000

    Use a barchart. Dat's bettah.

--- healthtracker/static/js/tracker.js
+++ healthtracker/static/js/tracker.js
@@ -1,16 +1,33 @@
 $(document).ready(function(){
     
     statuses = $(".statuses").data("statuses");
+    console.log(statuses);
+    var width = 900;
+    var height = 80;
+    var barWidth = 50;
 
-    // create an SVG element inside the #graph div that fills 100% of the div
-    var graph = d3.select(".statuses").append("svg:svg").attr("width", "100%").attr("height", "50px");
+    var chart = d3.select(".statuses").append("svg:svg").attr("width", width).attr("height", height);
 
-    var x = d3.scale.linear().domain([0, 10]).range([0, 500]);
-    var y = d3.scale.linear().domain([0, 5]).range([0, 20]);
+    var x = d3.scale.linear().domain([0, statuses.length]).range([0, width]);
+    var y = d3.scale.linear().domain([0, 5]).range([0, height]);
 
-    var line = d3.svg.line()
-        .x(function(d,i) { return x(i); })
-        .y(function(d) { return y(d); })
-    
-    graph.append("svg:path").attr("d", line(statuses));
+    chart.selectAll("rect")
+        .data(statuses)
+        .enter().append("rect")
+        .attr("x", function(d, i) { return x(i) - .5; })
+        .attr("y", function(d) { return height - y(d) - .5; })
+        .attr("width", barWidth)
+        .attr("height", function(d) { return y(d); });
+
+    chart.selectAll("text")
+        .data(statuses)
+        .enter().append("text")
+        .text(function(d) { return d; })
+        .attr("x", function(d, i) { return x(i) + barWidth/2; })
+        .attr("y", function(d) { return height - 6; })
+        .attr("text-anchor", "middle")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "14px")
+        .attr("fill", "black");
+
 });
