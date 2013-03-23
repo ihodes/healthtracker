0c198fe38ff289ee0d9693f99cb532447e2f9f3d
Isaac Hodes <ihodes@mac.com>
2013-03-23 11:39:44 -1000

    Fixed scheduled_mailer by wrapping it in the app's context... TK don't know that this is the best way of handling this.

--- scheduled_mailer.py
+++ scheduled_mailer.py
@@ -1,6 +1,9 @@
+from healthtracker import app
 from healthtracker.database import User
-from healthtracker.mailer import send_status_update_email
+import healthtracker.mailer as mailer
 
 if __name__ == '__main__':
-    for user in User.query.filter_by(is_confirmed=True, is_approved=True).all():
-        send_status_update_email(user)
+    with app.test_request_context():
+        for user in User.query.filter_by(is_confirmed=True, is_approved=True).all():
+            mailer.send_status_update_email(user)
+            app.logger.info("Sent status update to {}.".format(user.email))
