32b4c4a71ca8df90057ac3f0930369bc60e642d2
Isaac Hodes <ihodes@mac.com>
2013-05-10 14:30:12 -0400

    fix mailer

--- scheduled_mailer.py
+++ scheduled_mailer.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     with app.test_request_context():
         for user in User.query.filter_by(is_confirmed=True, is_approved=True).all():
             mailer.send_status_update_email(user)
-            current_app.logger.info("Sent status update to {}.".format(user.email))
+            app.logger.info("Sent status update to {}.".format(user.email))
