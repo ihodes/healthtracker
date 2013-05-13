04eb5c526496381abba96927add80bb659cc6577
Isaac Hodes <ihodes@mac.com>
2013-05-13 14:24:01 -0400

    SegmentIO is now SEGMENTIO_API_KEY, used in the base layout.

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ PORT=int(os.environ.get('PORT'))
 
 SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
 MAILGUN_API_KEY=os.environ.get('MAILGUN_API_KEY')
+SEGMENTIO_API_KEY=os.environ.get('SEGMENTIO_API_KEY')
 
 DEBUG=os.environ.get('DEBUG', False)
 ADMIN_EMAIL=os.environ.get('ADMIN_EMAIL')
